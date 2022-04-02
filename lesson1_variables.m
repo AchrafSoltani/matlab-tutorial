fbaad23d7cfd6c31421358471c7534fd1cbaa76a
Achraf Soltani <achraf.soltani@gmail.com>
2022-04-02 13:08:51 +0100

    first version

---
+++ lesson1_variables.m
@@ -0,0 +1,36 @@
+% Basic variables
+x1 = 16;
+x2 = sqrt(x1);
+x3 = x2/2;
+x4 = x2*x1;
+y1 = 1; y2 = 2; y3 = 3;
+
+% Doubles qnd floats
+initial_velocity = 0;
+acceleration = 9.8;
+time = 20;
+final_velocity = initial_velocity + acceleration * time;
+
+% format commands
+format long
+x = 7 + 10/3 + 5 ^ 1.2;
+format bank
+daily_wage = 177.45;
+weekly_wage = daily_wage * 6;
+format short e
+short = 4.678 * 4.9;
+short
+format rat
+rat = 4.678 * 4.9;
+rat
+
+%  vectors
+r = [7 8 9 10 11];
+t = [2, 3, 4, 5, 6];
+res = r + t
+c = [7;  8;  9;  10; 11]
+
+% Matrices
+m = [1 2 3; 4 5 6; 7 8 9]
+
+save data
